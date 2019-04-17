f09f9e160e6531573800c816b492acacf20d571a
Cloud User <ec2-user@ip-12-12-2-55.ap-northeast-2.compute.internal>
2019-04-17 03:30:11 +0000

    commit

--- roles/release/jar/deploy/templates/main.sh
+++ roles/release/jar/deploy/templates/main.sh
@@ -2,28 +2,28 @@
 PATH=/usr/local/jdk/bin:/usr/local/jdk/jre/bin:/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/opt/aws/bin:/root/bin
 PROJECT_NAME="{{ project }}"
 JAR_FILE=`ls {{ pkgroot }}/*.jar`
-PIDFILE="/data/app/run/{{ project }}.pid"
+PIDFILE="{{ pkgroot }}/{{ project }}.pid"
 JAVA_OPTS="{{ javaargs }}"
 
 
 if ! [ -f "$JAR_FILE" ]; then
-    echo "$JAR_FILE is not exits."
+    echo "ERROR: $JAR_FILE is not exits."
     exit 1
 fi
 
 stop(){
     if [ -s $PIDFILE ]; then
         PID=`cat $PIDFILE`
-        for ((i=1;i<5;i++));do
+        for ((i=1;i<100;i++));do
             if [ -x /proc/$PID ]; then
                 echo "INFO: Waiting for ${PROJECT_NAME} stopped ..."
-                if [ "$count" == "5" ]; then
-                    echo "Maximum number of retries exceeded, Quit."
+                if [ "$count" == "30" ]; then
+                    echo "ERROR: Maximum number of retries exceeded, Quit."
                     exit 1
                 fi
                 kill $PID; sleep 1
             else
-                echo "INFO: Stoped."
+                echo "Stopped {{ project }}."
                 > $PIDFILE
                 break
             fi
@@ -34,29 +34,32 @@ stop(){
 restart(){
     if [ -s $PIDFILE ];then
         PID=`cat $PIDFILE`
-        for ((i=1;i<5;i++));do
+        for ((i=1;i<100;i++));do
             if [ -x /proc/$PID ]; then
                 echo "INFO: Waiting for ${PROJECT_NAME} stopped ..."
-                if [ "$count" == "5" ]; then
-                    echo "Maximum number of retries exceeded, Quit."
+                if [ "$count" == "30" ]; then
+                    echo "ERROR: Maximum number of retries exceeded, Quit."
                     exit 1
                 fi
                 kill $PID; sleep 1
             else
-                echo "INFO: Stoped."
+                echo "Stopped {{ project }}."
                 > $PIDFILE
                 break
             fi
         done
-        nohup java -jar $JAR_FILE $JAVA_OPTS > /dev/null 2>&1 &
+        nohup java -jar $JAR_FILE $JAVA_OPTS  > /dev/null 2>&1 &
         sleep 1
-        PID="$!"
-        printf "$PID" > $PIDFILE
+        echo "Started {{ project }}."
+        #PID=`ps -ef|grep "{{ pkgname }}"|grep -v grep | awk '{print $2}'`
+        PID=$!
+        printf $PID > $PIDFILE
     else
         nohup java -jar $JAR_FILE $JAVA_OPTS  > /dev/null 2>&1 &
         sleep 1
-        PID="$!"
-        printf "$PID" > $PIDFILE
+        echo "Started {{ project }}."
+        PID=$!
+        printf $PID > $PIDFILE
     fi  
 }
 
