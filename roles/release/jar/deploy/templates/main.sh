adc41ffadd3946f71a2cd913baf5a57ac5309911
Cloud User <ec2-user@ip-12-12-2-55.ap-northeast-2.compute.internal>
2019-04-26 02:29:47 +0000

    commit

--- roles/release/jar/deploy/templates/main.sh
+++ roles/release/jar/deploy/templates/main.sh
@@ -4,6 +4,7 @@ PROJECT_NAME="{{ project }}"
 JAR_FILE=`ls {{ pkgroot }}/*.jar`
 PIDFILE="{{ pkgroot }}/{{ project }}.pid"
 JAVA_OPTS="{{ javaargs }}"
+STDOUT_FILE="{{ pkgroot }}/stdout.log"
 
 
 if ! [ -f "$JAR_FILE" ]; then
@@ -17,8 +18,8 @@ stop(){
         for ((i=1;i<100;i++));do
             if [ -x /proc/$PID ]; then
                 echo "INFO: Waiting for ${PROJECT_NAME} stopped ..."
-                if [ "$count" == "30" ]; then
-                    echo "ERROR: Maximum number of retries exceeded, Quit."
+                if [ "$i" == "30" ]; then
+                    echo "ERROR: Stop timeout, Quit."
                     exit 1
                 fi
                 kill $PID; sleep 1
@@ -31,14 +32,22 @@ stop(){
     fi
 }
 
+start() {
+    nohup java -jar $JAR_FILE $JAVA_OPTS  > $STDOUT_FILE 2>&1 &
+    sleep 1
+    echo "Started {{ project }}."
+    PID=$!
+    printf $PID > $PIDFILE
+}
+
 restart(){
     if [ -s $PIDFILE ];then
         PID=`cat $PIDFILE`
         for ((i=1;i<100;i++));do
             if [ -x /proc/$PID ]; then
                 echo "INFO: Waiting for ${PROJECT_NAME} stopped ..."
-                if [ "$count" == "30" ]; then
-                    echo "ERROR: Maximum number of retries exceeded, Quit."
+                if [ "$i" == "30" ]; then
+                    echo "ERROR: Stop timeout, Quit."
                     exit 1
                 fi
                 kill $PID; sleep 1
@@ -48,18 +57,9 @@ restart(){
                 break
             fi
         done
-        nohup java -jar $JAR_FILE $JAVA_OPTS  > /dev/null 2>&1 &
-        sleep 1
-        echo "Started {{ project }}."
-        #PID=`ps -ef|grep "{{ pkgname }}"|grep -v grep | awk '{print $2}'`
-        PID=$!
-        printf $PID > $PIDFILE
+        start
     else
-        nohup java -jar $JAR_FILE $JAVA_OPTS  > /dev/null 2>&1 &
-        sleep 1
-        echo "Started {{ project }}."
-        PID=$!
-        printf $PID > $PIDFILE
+        start
     fi  
 }
 
